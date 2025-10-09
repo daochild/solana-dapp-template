2c0a483a68fad9a182ea5336d242c07226d1920f
daochild <paul.bolhar@gmail.com>
2025-10-10 00:51:07 +0300

    expect NEXT_PUBLIC_PROJECT_ID defined

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
 
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID! // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
